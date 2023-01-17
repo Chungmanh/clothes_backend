982e9baa3a1adfddb80a9d25e5e3f61ae2cb44b4
Chungmanh <duongchung7100@gmail.com>
2023-01-18 00:17:04 +0700

    add mongodb url

--- backend/_utils/connectDb.js
+++ backend/_utils/connectDb.js
@@ -1,21 +1,16 @@
-const mongoose = require('mongoose');
-const db = require('../_config/db');
+const mongoose = require("mongoose");
+const db = require("../_config/db");
 
 async function connect() {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/shop_web', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('connect cuccessfully!!!')
-    } catch (error) {
-        console.log('connect failure!!!')
-    }
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connect cuccessfully!!!");
+  } catch (error) {
+    console.log("connect failure!!!");
+  }
 }
 
 module.exports = connect;
-
-
-
-
-
